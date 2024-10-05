99d5a1ecd5b4c0c5630b09bf72204fcfa70759ce
Emerson Batista <84859510+emersondev21@users.noreply.github.com>
2024-10-05 15:53:01 +0000

    Update assets

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,3 +5,24 @@ function activate_nav_mobile(params) {
   nav.classList.toggle('nav_mobile_active')
   nav_list.classList.toggle('nav_mobile_active')
 }
+
+function activate_personal(params) {
+  let personal = document.getElementById('personal_projects')
+
+  personal.classList.toggle('personal_projects_show')
+}
+
+
+const text = "Sou técnico em Análise e Desenvolvimento de sistemas, e atualmente, estou cursando Engenharia da Computação na UFRPE de Belo Jardim, PE. Estou constantemente atualizando meus conhecimentos e buscando novos desafios na área de tecnologia. Tenho paixão por aprender e aplicar esses conhecimentos para criar soluções inovadoras!";
+let index = 0;
+const speed = 50; // Velocidade da digitação (em ms)
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById("typing-text").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+window.onload = typeWriter;
