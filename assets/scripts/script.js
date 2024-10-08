ad6a291bb55f637064eb8d1f5c4bf8dded9d2f4b
Emerson Batista <84859510+emersondev21@users.noreply.github.com>
2024-10-08 04:20:00 +0000

    Adicionada Versão Mobile

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,23 +6,32 @@ function activate_nav_mobile(params) {
   nav_list.classList.toggle('nav_mobile_active')
 }
 
+function closeNav() {
+  let nav = document.getElementById('nav_mobile');
+  let nav_list = document.getElementById('mobile_nav_links');
+
+  // Remove a classe ativa e desmarca o checkbox
+  nav.classList.remove('nav_mobile_active');
+  nav_list.classList.remove('nav_mobile_active');
+  checkbox.checked = false;
+}
+
 function activate_personal(params) {
   let personal = document.getElementById('personal_projects')
 
   personal.classList.toggle('personal_projects_show')
 }
 
-
 const text = "Sou técnico em Análise e Desenvolvimento de sistemas, e atualmente, estou cursando Engenharia da Computação na UFRPE de Belo Jardim, PE. Estou constantemente atualizando meus conhecimentos e buscando novos desafios na área de tecnologia. Tenho paixão por aprender e aplicar esses conhecimentos para criar soluções inovadoras!";
 let index = 0;
 const speed = 50; // Velocidade da digitação (em ms)
 
 function typeWriter() {
-    if (index < text.length) {
-        document.getElementById("typing-text").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, speed);
-    }
+  if (index < text.length) {
+    document.getElementById("typing-text").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
 }
 
 window.onload = typeWriter;
